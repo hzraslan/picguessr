2a5f0f70b30d465f2624bc2ef9db8b825a32b1f4
samcohen <sam@obsessvr.com>
2024-10-01 11:41:51 -0400

    adding initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import exifr from "exifr";
 
-function App() {
+// const localS = (file) => window.localStorage.setItem("img", file.name);
+export default function App() {
+  const [file, setFile] = useState("");
+  const [imgSrc, setImgSrc] = useState();
+  const [loading, setLoading] = useState(true);
+  // const [gps, setGps] = useState({ latitude: 0, longitude: 0 });
+  // const currentRef = useRef();
+  const handleChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const readFile = async () => {
+    const reader = new FileReader();
+
+    await reader.addEventListener(
+      "load",
+      () => {
+        // console.log(reader.result);
+        setImgSrc(reader.result);
+        setLoading(false);
+      },
+      false
+    );
+    if (file) {
+      await reader.readAsDataURL(file);
+
+      try {
+        let { latitude, longitude } = await exifr.gps(file);
+        console.log(latitude, longitude);
+      } catch (e) {
+        console.error('"No gps Meta Data :) Try another pix"');
+        throw new Error("No gps Meta Data :) Try another pix");
+      }
+    }
+  };
+  useEffect(() => {
+    readFile();
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Load an Image</h1>
+      <h2>only React</h2>
+      <input type="file" onChange={handleChange} />
+
+      <div>{!loading && <img id="output" src={imgSrc} />}</div>
     </div>
   );
 }
-
-export default App;
