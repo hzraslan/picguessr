66edaefb6fbc0b75187bfa71b444478da47868ef
hizir <hizir@obsessvr.com>
2024-10-01 12:29:43 -0400

    update

--- src/App.js
+++ src/App.js
@@ -49,3 +49,24 @@ export default function App() {
     </div>
   );
 }
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDr1nrXZlu5zHz_OJ8NtlTDWbwvNvx3vfY",
+//   authDomain: "picguessr-74fb2.firebaseapp.com",
+//   projectId: "picguessr-74fb2",
+//   storageBucket: "picguessr-74fb2.appspot.com",
+//   messagingSenderId: "1019944570937",
+//   appId: "1:1019944570937:web:4b5d7463f7eedf0b6b3de8",
+//   measurementId: "G-6V41DLDZ7Y"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
